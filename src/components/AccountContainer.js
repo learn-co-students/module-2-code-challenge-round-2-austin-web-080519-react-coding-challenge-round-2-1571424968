1c368739d8b2c4dd7ca85d958a96218ef3d3fcd6
Cody <lawdermiltcody@gmail.com>
2019-10-18 15:13:03 -0500

    submission for the 2nd code challenge

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,60 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+// import { transactions } from "../transactionsData";
 
 class AccountContainer extends Component {
-
-  constructor() {
-    super()
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      myTransData: [],
+      inputValue: ""
+    };
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
   }
 
-  handleChange(event) {
-    // your code here
+  fetchingData = () => {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+      .then(resp => resp.json())
+      .then(myTransData => {
+        const inputLowerCase = this.state.inputValue.toLowerCase();
+
+        // console.log("fetchingData", resp);
+        const filteredData = myTransData.filter(data => {
+          let myData =
+            data.description.toLowerCase() + data.category.toLowerCase();
+          return myData.includes(inputLowerCase);
+        });
+        this.setState({
+          myTransData: filteredData
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.fetchingData();
   }
+
+  handleChange = input => {
+    // console.log(input);
+    this.setState({
+      inputValue: input
+    });
+    this.fetchingData();
+  };
 
   render() {
-
+    // console.log("this.state", this.props);
+    console.log();
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList myTransData={this.state.myTransData} />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,31 @@
-import React from 'react'
+import React from "react";
+import Transaction from "./Transaction";
 
-const TransactionsList = () => {
-
+const TransactionsList = props => {
+  // console.log(props.myTransData);
   return (
     <table className="ui celled striped padded table">
       <tbody>
         <tr>
           <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
+            <h3 className="ui center aligned header">Posted At</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
+            <h3 className="ui center aligned header">Description</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
+            <h3 className="ui center aligned header">Category</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
+            <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-
-        {"... your code here..."}
-
+        {props.myTransData.map(trans => (
+          <Transaction key={trans.id} myTransData={trans} />
+        ))}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default TransactionsList
+export default TransactionsList;
