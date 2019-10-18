afe1277751b5e60d51d670895b039cb3c1680859
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-10-18 15:42:18 -0500

    submission for deadline

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,54 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+// import { transactions } from "../transactionsData";
 
 class AccountContainer extends Component {
-
-  constructor() {
-    super()
-
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: [],
+      searchInput: ""
+    };
+  }
+  componentDidMount() {
+    this.fetchTransactions();
   }
 
-  handleChange(event) {
-    // your code here
-  }
+  fetchTransactions = () => {
+    fetch(`https://boiling-brook-94902.herokuapp.com/transactions`)
+      .then(resp => resp.json())
+      .then(allTransactions => {
+        // console.log(allTransactions);
+        const lowerCaseTransactions = this.state.searchInput.toLowerCase();
+        const filteredTransactions = allTransactions.filter(transaction => {
+          let transactionsCategory = transaction.category.toLowerCase();
+          let transactionsDescription = transaction.description.toLowerCase();
+          return (transactionsCategory, transactionsDescription).includes(
+            lowerCaseTransactions
+          );
+        });
+        this.setState({
+          transactions: filteredTransactions
+        });
+      });
+  };
+  handleChange = input => {
+    // console.log("search type", input);
+    this.setState({
+      searchInput: input
+    });
+    this.fetchTransactions();
+  };
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList allTransactions={this.state.transactions} />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,37 @@
-import React from 'react'
+import React from "react";
 
-const TransactionsList = () => {
+const TransactionsList = props => {
+  // console.log(props);
 
   return (
     <table className="ui celled striped padded table">
       <tbody>
         <tr>
           <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
+            <h3 className="ui center aligned header">Posted At</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
+            <h3 className="ui center aligned header">Description</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
+            <h3 className="ui center aligned header">Category</h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
+            <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
 
-        {"... your code here..."}
-
+        {props.allTransactions.map(trans => (
+          <tr>
+            <td>{trans.posted_at}</td>
+            <td>{trans.description}</td>
+            <td>{trans.category}</td>
+            <td>{trans.amount}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default TransactionsList
+export default TransactionsList;
